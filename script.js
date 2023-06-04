cad8bd35e2b965c026120a4ae7c592f94b0c65f7
franc51 <101558212+franc51@users.noreply.github.com>
2023-06-04 15:54:31 +0300

    added first page

---
+++ script.js
@@ -0,0 +1,6 @@
+
+gsap.from('.title', { duration: 1, x: '-200%'})
+gsap.from('.designed', { duration: 1.2, x: '-200%'})
+gsap.from('.buttons-div', { duration: 1.5, x: '-200%'})
+gsap.from('.right-container', { duration: 1.5, opacity: 0, x: '+100%'})
+gsap.to('.right-container', { duration: 2, opacity: 1,})
